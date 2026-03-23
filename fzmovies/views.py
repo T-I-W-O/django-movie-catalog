2e90c3c04d9ae0e1cf538b92a744e5bea2231666
T.I.W.O <tiwoadex@gmail.com>
2026-03-23 20:50:23 +0100

    Change rendered template from fzmovies.html to index.html

--- fzmovies/views.py
+++ fzmovies/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def fz(request):
-    return render(request, 'fzmovies.html')
+    return render(request, 'index.html')
 
 def wonder(request):
     return render(request, 'wonder.html')
